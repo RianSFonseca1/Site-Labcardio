966dc8f1a9d497006689d3488852715d88fe63d1
RianSFonseca1 <riansf.acc1@gmail.com>
2024-01-30 20:35:17 -0300

    Primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        'Laranja-1': '#ffcc33',
+        'Laranja-2': '#ffa832',
+        'Laranja-3': '#fe8330',
+        'Laranja-4': '#fe5f2f',
+        'Laranja-5': '#fd3a2d',
+      },
+
+      spacing: {
+        '128': '32rem',
+      },
+    },
+  },
+
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
